3536ed5736e1b367b6d38feb761bba7487739afe
effert <effert111222@gmail.com>
2024-01-04 13:52:18 +0800

    feat: add link to chat page in layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Locale } from '@/lib/utils/get-dictionary';
 import SwitchTheme from '@/components/layout/switch-theme';
 import SwitchLang from '@/components/layout/switch-lang';
+import Link from 'next/link';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -14,7 +15,9 @@ const Layout = ({ children, lang }: LayoutProps) => {
       <div className="sticky top-0 z-50 w-full transition-colors duration-500 shadow-md bg-white dark:bg-black dark:text-white">
         <div className="max-w-8xl mx-auto">
           <div className="flex justify-between items-center py-4 lg:px-8 mx-4 lg:mx-0">
-            <div className="text-2xl font-semibold">NOOK</div>
+            <Link href="/chat">
+              <div className="text-2xl font-semibold">NOOK</div>
+            </Link>
             <div className="flex items-center gap-4">
               <SwitchTheme />
               <SwitchLang lang={lang} />
