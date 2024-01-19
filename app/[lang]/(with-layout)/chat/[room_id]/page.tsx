a746281d25d63b9b4ffe349e209e171cd48d443a
effert <effert111222@gmail.com>
2024-01-19 17:44:18 +0800

    fix: prisma

--- app/[lang]/(with-layout)/chat/[room_id]/page.tsx
+++ app/[lang]/(with-layout)/chat/[room_id]/page.tsx
@@ -14,9 +14,10 @@ import UserInfo from './user-info';
 import RoomListWrap from './room-list-wrap';
 import RoomNameH5 from './room-name-h5';
 
+const cookieStore = cookies();
+const authorization = cookieStore.get(TOKEN);
+
 async function fetchUserInfo() {
-  const cookieStore = cookies();
-  const authorization = cookieStore.get(TOKEN);
   if (authorization) {
     const token = authorization.value;
     let resp = await fetch(`${process.env.BASE_URL}/user-info`, {
@@ -33,8 +34,6 @@ async function fetchUserInfo() {
 }
 
 async function getRoomList() {
-  const cookieStore = cookies();
-  const authorization = cookieStore.get(TOKEN);
   if (authorization) {
     const token = authorization.value;
     let resp = await fetch(`${process.env.BASE_URL}/user/rooms`, {
